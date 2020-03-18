2c162d43987aa58ba1ce3c021cf9c782ed44c939
btekl <brian.teklits@gmail.com>
2020-03-18 16:46:29 -0600

    Player name moves with character, WASD movement, smoother/more responsive movement

--- 5-phaser/src/scenes/gamescene.js
+++ 5-phaser/src/scenes/gamescene.js
@@ -3,24 +3,31 @@ import cBG from "../assets/casinoBG.jpg";
 import table from "../assets/Table.png";
 import vwall from "../assets/VerticalWall.png";
 import dude from "../assets/sq.png";
+import arcade from "../assets/arcade.png"
 
 export default class GameScene extends Phaser.Scene {
     constructor () {
       super('game');            // string is unique scene key name
+
+      this.player_sprite = null;
+      this.player_name = null;
       this.player = null;
+
       this.platforms = null;
+      this.arcade = null;
+
       this.cursors = null;
+      this.text_test = null;
+      this.action_key = null;
     }
     
     preload ()
     {
+        this.load.image('arcade', arcade);
         this.load.image('casino', cBG);
         this.load.image('blackjack', table);
         this.load.image('vwall', vwall);
-        this.load.spritesheet('dude', 
-            dude,
-            { frameWidth: 75, frameHeight: 75 }
-        );
+        this.load.image('dude', dude);
     }
 
     create ()
@@ -28,56 +35,91 @@ export default class GameScene extends Phaser.Scene {
         this.add.image(400, 300, 'casino');
 
         this.platforms = this.physics.add.staticGroup();
+        this.arcade = this.physics.add.staticGroup();
+        this.arcade.create(750,50,'arcade');
 
         this.platforms.create(400, 632, 'ground').setScale(2).refreshBody();
 
         this.platforms.create(832, 225, 'vwall').setScale(2).refreshBody();
         this.platforms.create(400, -32, 'ground').setScale(2).refreshBody();
         this.platforms.create(100, 450, 'blackjack');
-        this.player = this.physics.add.sprite(759, 300, 'dude').setScrollFactor(0);
 
-     //   player.setBounce(0.2);
-    //    player.setCollideWorldBounds(true);
-     //   player.body.setGravityY(300)
+        /*
+        Attach player name to player sprite
+        coordinates of sprite/name are relative to position in container
+        container coordinates are where the container object is placed in the scene
+        container size defaults to 64x64 if not set, must enable physics to be able to move
+        */
+        this.player_sprite = this.physics.add.sprite(0, 0, 'dude');
+        this.player_name = this.add.text(-30,-50, "Player", {color: '#FFFFFF'});
+        this.player = this.add.container(750,300, [this.player_sprite, this.player_name]);
+        this.player.setSize(70,70);
+        this.physics.world.enable(this.player);
+
+        /*
+        Adds collision between player/platform objects
+        Can add function argument to collider to do something on collision (scene change etc.)
+        */
         this.physics.add.collider(this.player, this.platforms);
-        this.cursors = this.input.keyboard.createCursorKeys();
+        /*
+        this.physics.add.collider(this.player, this.arcade, 
+            function() {
+                    this.text_test.setText("WOO");
+                
+            }, this);
+
+        this.text_test = this.add.text(100, 500, "Test", { color: '#00ff00' });*/
+
+        // WASD movement keys
+
+        this.cursors = this.input.keyboard.addKeys(
+            {up:Phaser.Input.Keyboard.KeyCodes.W,
+            down:Phaser.Input.Keyboard.KeyCodes.S,
+            left:Phaser.Input.Keyboard.KeyCodes.A,
+            right:Phaser.Input.Keyboard.KeyCodes.D}
+            );
+
+            
+    
         
     }
 
     update ()
     {
+        // Seperating x and y movement results in smoother control
+        this.player.body.setVelocity(0,0);
+
         if (this.cursors.left.isDown)
         {
-            this.player.setVelocityX(-160);
+            this.player.body.setVelocityX(-160);
 
-         //   player.anims.play('left', true);
         }
         else if (this.cursors.right.isDown)
         {
-            this.player.setVelocityX(160);
+            this.player.body.setVelocityX(160);
 
-         //   player.anims.play('right', true);
         }
-        else if (this.cursors.up.isDown) //&& player.body.touching.down)
+
+        if (this.cursors.up.isDown) 
         {
-            this.player.setVelocityY(-160);
+            this.player.body.setVelocityY(-160);
             
         }
-        else if (this.cursors.down.isDown) //&& player.body.touching.down)
+        else if (this.cursors.down.isDown) 
         {
-            this.player.setVelocityY(160);
+            this.player.body.setVelocityY(160);
         }
-        else
-        {
-            this.player.setVelocityX(0);
-            this.player.setVelocityY(0);
+        
 
-         //   player.anims.play('turn');
-        }
-
+        /* Testing different scene transitions
         if(this.player.x < -50)
             this.scene.start('blackjack', );
 
-        
+        this.input.keyboard.on('keydown_F', function (event) {
+
+            this.text_test.setText("WOO")
+
+        }, this); */
+
     }
   };
