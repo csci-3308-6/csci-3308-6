b132f9ccc964270d7af4f44533ace84ff73990e4
btekl <brian.teklits@gmail.com>
2020-03-23 20:33:17 -0600

    Removed old references in index

--- 5-phaser/src/index.js
+++ 5-phaser/src/index.js
@@ -2,7 +2,6 @@ import Phaser from "phaser";
 import GameScene from "./scenes/gamescene";
 import config from "./config/config";
 import BlackJack from "./scenes/blackjack";
-import testScene from "./scenes/test";
 import PreloadScene from "./scenes/preload";
 
 
@@ -12,7 +11,6 @@ class Game extends Phaser.Game {
     this.scene.add('preload', PreloadScene);
     this.scene.add('game', GameScene);
     this.scene.add('blackjack', BlackJack);
-    this.scene.add('test', testScene);
     this.scene.start('preload');
   }
 }
