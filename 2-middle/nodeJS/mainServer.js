82d2459455a955bc187352b28acda91c8815e41d
nattobiason <nato0933@colorado.edu>
2020-04-21 13:31:35 -0600

    updating files

--- 2-middle/nodeJS/mainServer.js
+++ 2-middle/nodeJS/mainServer.js
@@ -1,7 +1,3 @@
-// node.js connection to Postgres database
-// website I got info from to make this:
-// https://blog.logrocket.com/setting-up-a-restful-api-with-node-js-and-postgresql-d96d6fc892d8/
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
@@ -23,26 +19,8 @@ const db= new Pool({
 
 app.use(express.static(__dirname + '/'));
 
-//Questions:
-//Front End:
-//	How is avatar images implemented?
-//	Are we still changing colors/shapes?
-//	What is gonna be home page?
-//	Avatar setup page?
-//	More Game Tables?
-//	How many games are going to be implemented?
-//	BlackJack:
-//		Where are losses and money changes being stored?
-//
-//Back End:
-//	What is user_token used for?
 
-//TO-DO FUNCTIONS:
-//Sign In table:users            Brehn 
-//Post Game Stats table:stats    
-//Get Money Values from Database 
-//Get Colors from Database
-//Avatar Shape Color return? table:display
+/* sign-up */
 
 app.get('/', function(req, res){
   	res.sendFile('./views/sign-up.html',{
@@ -53,12 +31,45 @@ app.get('/', function(req, res){
 app.post('/', function(req, res){
 	var email = req.body.email;
 	var pwd   = req.body.password;
-	var query = 'INSERT INTO users (username, user_password) VALUES (\''+email+'\', \''+pwd+'\');'
+	var query = 'INSERT INTO users (username, user_password) VALUES (\''+ email +'\', \''+ pwd +'\');'
 	db.query(query)
   	res.sendFile('./views/sign-up.html',{
 		root:__dirname
 	});
 });
+
+/* sign-in */
+
+var currentUserID =  ;
+
+
+
+/*
+  charcter-customization.html --> game-room.js --> game.html
+
+  store color choice and display name from customization.html
+  saves that information
+  game-room.js needs to get that information when game.html loads
+*/
+
+app.get('/', function(req, res){
+  	res.sendFile('./views/charcter-customization.html',{
+		root:__dirname
+	});
+});
+app.post('/', function(req, res){
+  var name = req.body.characterName;
+  var color = req.body.characterColor;
+  var query = 'UPDATE display SET display_name = ' + name + ', shape = square, color = ' + color + ' WHERE display_id = ' + displayID + ';'
+  db.query(query)
+  	res.sendFile('./views/charcter-customization.html',{
+		root:__dirname
+	});
+});
+
+/* black-jack */
+
+/* game-room */
 
 
 
