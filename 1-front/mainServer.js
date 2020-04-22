266ea91f381de98c336c7d0ad19cf6034fa17413
nattobiason <nato0933@colorado.edu>
2020-04-22 12:42:19 -0600

    cookie update

--- 1-front/mainServer.js
+++ 1-front/mainServer.js
@@ -2,145 +2,82 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 var session = require('express-session')
+var pgp = require('pg-promise')();
 const app = express()
 const port = 3000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true,}))
 app.use(cookieParser())
-
-var pgp = require('pg-promise')();
+app.use(express.static(__dirname + '/'));
 
 const Pool = require('pg').Pool
-const db= new Pool({
-  user: 'postgres',
+const db = new Pool({
+  user: 'nattobiason',
   host: 'localhost',
   database: 'casino',
   password: 'password',
   port: 5432,
 })
 
-app.use(express.static(__dirname + '/'));
 
 /* Cookie */
-app.use(session({
-    key: 'user_sid',
-    secret: 'somerandonstuffs',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        expires: 600000
-    }
-}));
-app.use((req, res, next) => {
-    if (req.cookies.user_sid && !req.session.user) {
-        res.clearCookie('user_sid');
-    }
-    next();
+var user = {
+  userID: "",
+  displayID: "",
+  statsID: ""
+}
+app.get('/setuser', function(req, res){
+  var user = 'SELECT user_ID FROM users WHERE user_ID = '+ user.userID +';';
+  var display = 'SELECT display_ID FROM display WHERE display_ID = '+ user.displayID +';';
+  res.cookie("userData", user);
+  res.send('user data added to cookie');
 });
-var sessionChecker = (req, res, next) => {
-    if (req.session.user && req.cookies.user_sid) {
-        res.redirect('/views/index.html');
-    } else {
-        next();
-    }
-};
 
 /*Variables*/
 
-var userID = 1;
-var displayID = 1;
 
-/*Home Page*/
-app.get('/', sessionChecker, (req, res) => {
-    res.redirect('/home');
-});
-app.route('/home')
-    .get(sessionChecker, (req, res) => {
-        res.sendFile('/views/index.html',{
-        		root:__dirname
-        	});
-    })
-// app.get('/', function(req, res){
-//   	res.sendFile('./views/index.html',{
-// 		root:__dirname
-// 	});
-// });
-
-
-/* sign-up */
-app.route('/sign-up')
-    .get(sessionChecker, (req, res) => {
-        res.sendFile('/views/sign-up.html',{
-        		root:__dirname
-        	});
-    })
-    .post((req, res) => {
-      var email = req.body.email;
-      var pwd   = req.body.password;
-      var query = 'INSERT INTO users (username, user_password) VALUES (\''+ email +'\', \''+ pwd +'\');'
-      db.query(query)
-        .then(user => {
-            req.session.user = user.dataValues;
-            res.redirect('/sign-upcc');
-        })
-        .catch(error => {
-            res.redirect('/sign-up');
-        });
-    });
-
-// app.get('/sign-up', sessionChecker, function(req, res){
-//   	res.sendFile('./views/sign-up.html',{
-// 		root:__dirname
-// 	});
-// });
-//
-// app.post('/sign-up', sessionChecker, function(req, res){
-// 	currentUserID = req.body.email;
-// 	var email = req.body.email;
-// 	var pwd   = req.body.password;
-// 	var query = 'INSERT INTO users (username, user_password) VALUES (\''+ email +'\', \''+ pwd +'\');'
-// 	db.query(query)
-//   	res.sendFile('./views/character-customization.html',{
-// 		root:__dirname
-// 	});
-// });
-
-
-
-/* sign-in */
-app.route('/sign-in')
-    .get(sessionChecker, (req, res) => {
-      res.sendFile('/views/sign-up.html',{
-          root:__dirname
-        });
-  })
-    .post((req, res) => {
-        var username = req.body.inputEmail,
-            password = req.body.inputPassword;
-        var query = "SELECT * FROM users WHERE username = "+ username +", userpassword = "+ password +";"
-        // db.query(query)
-        // if(query){ // this check should probably be changed somehow idk
-        //   res.redirect('/game-room')
-        // }
-        // else{
-        //   res.redirect('/sign-in')
-        // }
-    });
-
-
-/* character customization */
-
-app.post('/sign-upcc', function(req, res){
-  var name = req.body.characterName;
-  var color = req.body.characterColor.value;
-  console.log(color);
-  var query = 'INSERT INTO display (display_id, display_name, shape, color) VALUES ('+ displayID +', '+ name +', square, '+ color +');'
-  db.query(query)
-  	res.redirect('/views/game.html',{
+/* basic route to home page */
+app.get('/', function(req, res){
+  	res.sendFile('./views/index.html',{
 		root:__dirname
 	});
 });
+
+/* sign-up */
+app.get('/sign-up', function(req, res){
+  	res.sendFile('/views/sign-up.html',{
+		root:__dirname
+	});
+});
+app.post('/sign-up', function(req, res){
+	user.userID = req.body.email;
+	var email = req.body.email;
+	var pwd   = req.body.password;
+	var query = 'INSERT INTO users (username, user_password) VALUES (\''+ email +'\', \''+ pwd +'\');'
+	db.query(query)
+  	res.sendFile('/views/character-customization.html',{
+		root:__dirname
+	});
+});
+
+/* character customization */
+// app.get('/sign-upcc', function(req, res){
+//   var query = 'SELECT user_ID FROM users WHERE username = '+ user.userID +');'
+//   db.query(query)
+//   //user.userID = query.user_ID;
+//   console.log(query);
+// });
+app.post('/sign-up/cc', function(req, res){
+  var name = req.body.characterName;
+  var color = req.body.characterColor.value;
+  var query = 'INSERT INTO display (display_ID, display_name, shape, color) VALUES (2, \''+ name +'\', \'square\', \''+ color +'\');'
+  db.query(query)
+  	res.sendFile('/views/sign-in.html',{
+		root:__dirname
+	});
+});
+
 
 
 /* black-jack */
@@ -149,15 +86,10 @@ app.post('/sign-upcc', function(req, res){
 
 
 
-/* sign-out */
-
+/* sign-out -- delete cookies */
 app.get('/sign-out', (req, res) => {
-    if (req.session.user && req.cookies.user_sid) {
-        res.clearCookie('user_sid');
-        res.redirect('/');
-    } else {
-        res.redirect('/sign-in');
-    }
+  res.clearCookie('userID');
+  res.clearCookie('displayID');
 });
 
 
